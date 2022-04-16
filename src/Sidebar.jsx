f299ca0f2e0b7e09453f516a183def18c4dca696
Olya <ola.shy@yahoo.com>
2022-04-16 16:43:04 +0200

    fix

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Sidebar.css';
-
+import { Avatar } from '@mui/material';
 function Sidebar() {
   return (
     <div className='sidebar'>
-      <div className='sidebar__header'></div> 
+      <div className='sidebar__header'>
+        <Avatar />
+      </div> 
       <div className='search'></div>     
       <div className="sidebar__chats"></div>
     </div>
