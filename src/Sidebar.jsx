7a8acf759ec9b296e088f4e8d8ee1f949d7ebd19
Olya <ola.shy@yahoo.com>
2022-04-16 21:26:00 +0200

    fix

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Sidebar.css';
-import { Avatar } from '@mui/material';
+import { Avatar, IconButton } from '@mui/material';
+import ChatIcon from '@mui/icons-material/Chat';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 function Sidebar() {
   return (
     <div className='sidebar'>
       <div className='sidebar__header'>
         <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton><DonutLargeIcon /></IconButton>
+          <IconButton><ChatIcon /></IconButton>   
+          <IconButton><MoreVertIcon /></IconButton>          
+        </div>
       </div> 
       <div className='search'></div>     
       <div className="sidebar__chats"></div>
