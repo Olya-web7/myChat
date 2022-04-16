5aaa8d1a094d90674ec2c0094f1f90464024d35c
Olya <ola.shy@yahoo.com>
2022-04-16 23:15:09 +0200

    fix

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,12 +1,31 @@
-import { Avatar } from '@mui/material';
-import React from 'react';
+import { Avatar, IconButton } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import './Chat.css';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 function Chat() {
+  const [seed, setSeed] = useState('');
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 500));
+  }, []);
+
   return (
     <div className='chat'>
       <div className="chat__header">
-        <Avatar />
+        <Avatar src={`https://avatars.dicebear.com/api/croodles/${seed}.svg`}/>
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton><SearchOutlinedIcon /></IconButton>
+          <IconButton><AttachFileIcon /></IconButton>
+          <IconButton><MoreVertIcon /></IconButton>
+        </div>
       </div>
       <div className="body"></div>
       <div className="chat__footer"></div>
