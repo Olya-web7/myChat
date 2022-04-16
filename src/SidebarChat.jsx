58116af90b63c253e01c36680e8eccdbae03e25d
Olya <ola.shy@yahoo.com>
2022-04-16 22:49:55 +0200

    fix

--- src/SidebarChat.jsx
+++ src/SidebarChat.jsx
@@ -2,20 +2,33 @@ import { Avatar } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import './SidebarChat.css';
 
-function SidebarChat() {
+function SidebarChat({ addNewChat}) {
   const [seed, setSeed] = useState('');
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 500));
   }, []);
 
-  return (
+  const createChat = () => {
+    const roomName = prompt("Please enter name for chat");
+
+    if (roomName) {
+      //to do
+    }
+  };
+
+  return !addNewChat ? (
     <div className='sidebarChat'>
       <Avatar src={`https://avatars.dicebear.com/api/croodles/${seed}.svg`}/>
       <div className="sidebarChat__info">
-        <h2>room name</h2>
+        <h3>room name</h3>
         <p>last message ...</p>
       </div>
+    </div>
+  ) : (
+    <div onClick={createChat}
+      className='sidebarChat'>
+      <h2>Add new chat</h2>
     </div>
   );
 }
