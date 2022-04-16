7727e55bcb99feb263c3e23826f7824af6a6c452
Olya <ola.shy@yahoo.com>
2022-04-16 22:56:33 +0200

    fix

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,10 +1,17 @@
+import { Avatar } from '@mui/material';
 import React from 'react';
 import './Chat.css';
 
 function Chat() {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="chat__header">
+        <Avatar />
+      </div>
+      <div className="body"></div>
+      <div className="chat__footer"></div>
+    </div>
   )
 }
 
-export default Chat
+export default Chat;
