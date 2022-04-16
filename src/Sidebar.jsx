1d1e1b08ed47c687a4cb2e3424f8698a78585ed1
Olya <ola.shy@yahoo.com>
2022-04-16 21:37:16 +0200

    fix

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -4,6 +4,7 @@ import { Avatar, IconButton } from '@mui/material';
 import ChatIcon from '@mui/icons-material/Chat';
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 function Sidebar() {
   return (
@@ -16,10 +17,17 @@ function Sidebar() {
           <IconButton><MoreVertIcon /></IconButton>          
         </div>
       </div> 
-      <div className='search'></div>     
+
+      <div className='sidebar__search'>
+        <div className="sidebar__searcContainer">
+          <SearchOutlinedIcon />
+          <input placeholder='search' type='text' />
+        </div>
+      </div>     
+
       <div className="sidebar__chats"></div>
     </div>
-  )
+  );
 }
 
 export default Sidebar
