2381aea894634c6883deb3486f9d5743bdb452c6
Olya <ola.shy@yahoo.com>
2022-04-25 19:50:16 +0200

    fix

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import './Sidebar.css';
+import React, { useEffect, useState } from 'react';
 import { Avatar, IconButton } from '@mui/material';
-import ChatIcon from '@mui/icons-material/Chat';
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import './Sidebar.css';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import SidebarChat from './SidebarChat';
+import db from "./firebase";
 
 function Sidebar() {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    db.collection('rooms').onSnapshot((snapshot) => 
+      setRooms(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data()
+        }))
+      )
+    );
+  }, [])
 
   return (
     <div className='sidebar'>
@@ -29,6 +42,10 @@ function Sidebar() {
 
       <div className="sidebar__chats">
         <SidebarChat addNewChat/>
+        {rooms.map((room) => (
+          <SidebarChat key={room.id} id={room.id}
+          name={room.data.name} />
+        ))}
       </div>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
-import firebase from 'firebase';
+// import firebase from 'firebase';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBAxohQ0F123cBe0KgqAVhWj0uqWyOIc_A",
