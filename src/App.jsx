9523dbf4eb940b531aa3dc829bebc787f2d67494
Olya <ola.shy@yahoo.com>
2022-04-25 20:31:39 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="app">
       <div className="app__body">
-        <Sidebar />
-        <Chat />
+        <Router>
+          <Switch>
+            <Route path='/app'>
+              <Sidebar />
+              <Chat />
+            </Route>
+            <Route path='/'>
+              <h1>home</h1>
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </div>
   );
