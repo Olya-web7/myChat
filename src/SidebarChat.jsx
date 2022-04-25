3431dff78bc2fdf1067d58af32394ad58bf2a20b
Olya <ola.shy@yahoo.com>
2022-04-25 20:05:05 +0200

    fix

--- src/SidebarChat.jsx
+++ src/SidebarChat.jsx
@@ -1,6 +1,7 @@
 import { Avatar } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import './SidebarChat.css';
+import db from './firebase';
 
 function SidebarChat({ id, name, addNewChat}) {
   const [seed, setSeed] = useState('');
@@ -13,7 +14,9 @@ function SidebarChat({ id, name, addNewChat}) {
     const roomName = prompt("Please enter name for chat");
 
     if (roomName) {
-      //to do
+      db.collection('rooms').add({
+        name: roomName,
+      });
     }
   };
 
