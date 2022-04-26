18be2b10be6e5eabed14d9e5e15c3b2388635de8
Olya <ola.shy@yahoo.com>
2022-04-26 20:21:14 +0200

    fix

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,15 +1,18 @@
 import { Button } from '@mui/material';
 import React from 'react';
+import { auth, provider } from './firebase';
 import './Login.css'
 
 function Login() {
-
-  const signIn = () => {};
+  const signIn = () => {
+    auth.signInWithPopup(provider)
+      .then(result => console.log(result))
+      .catch(error => alert(error.message));
+  };
 
   return (
     <div className='login'>
       <div className="login__container">
-        {/* <img src="" alt="" /> */}
         <div className="login__text">
           <h1>Sign in to MyChat</h1>
         </div>
