4d47509dc321a0de72813baa90756da216768dc1
Olya <ola.shy@yahoo.com>
2022-04-26 22:04:29 +0200

    fix

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -11,6 +11,7 @@ function Chat() {
   const [seed, setSeed] = useState('');
   const { roomId } = useParams();
   const [roomName, setRoomName] = useState('');
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     if (roomId) {
@@ -18,8 +19,16 @@ function Chat() {
         .doc(roomId)
         .onSnapshot(onSnapshot => setRoomName
           (onSnapshot.data().name));
+
+      db.collection('rooms')
+        .doc(roomId)
+        .collection('messages').orderBy('timestamp', 'asc')
+        .onSnapshot(snapshot => 
+          setMessages(snapshot.docs.map(doc => 
+          doc.data()))
+        );
     }
-  }, [roomId])
+  }, [roomId]);
 
 
   useEffect(() => {
@@ -43,11 +52,12 @@ function Chat() {
 
       </div>
       <div className="chat__body">
-        <p className={`chat__message ${true && 'chat__receiver'}`}>
-          <span className="chat__name">Sunny</span>
-          {`${'Hey guys'}`}
-          <span className="chat__timestamp">21:21</span>
-        </p>        
+        {messages.map(message => (
+          <p className={`chat__message ${true && 'chat__receiver'}`}>
+            {message.message}
+            <span className="chat__timestamp">21:21</span>
+          </p>        
+        ))}
       </div>
       <div className="chat__footer">
         <InsertEmoticonIcon />
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -19,6 +19,10 @@ function Sidebar() {
         }))
       )
     );
+
+    // return () => {
+    //   unsubscribe();
+    // };
   }, [])
 
   return (
